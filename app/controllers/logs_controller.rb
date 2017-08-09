dbbc474216c88c96c9bdc42ef53e8753a9684c00
Dustin Essington <aetaric@gmail.com>
2017-08-09 03:04:00 -0400

    fix for logs controller not rendering logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,8 +1,8 @@
 class LogsController < ApplicationController
   def show
-    @mods = Mods.find_by nick: => current_user.name
+    @mods = Mods.find_by :nick => current_user.name
     if @mods.channels.include? params[:channel]
-      @logs = Logs.find_by channel: => params[:channel]
+      @logs = Logs.find_by :channel => params[:channel]
     else
       redirect_to '/'
     end

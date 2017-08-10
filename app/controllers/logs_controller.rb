4d654c086d85e3b4f01f3a4296b9253d152ed157
Dustin Essington <aetaric@gmail.com>
2017-08-10 03:24:29 -0400

    Update to bootstrap 4 alpha6 navbar

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,10 +1,14 @@
 class LogsController < ApplicationController
   def show
-    @mods = Mods.find_by :nick => current_user.name
-    if @mods.channels.include? params[:channel]  || params[:channel] == current_user.name
-      @logs = Logs.find_by :channel => params[:channel]
-    else
-      redirect_to '/'
+    if !current_user.nil?
+      @mods = Mods.find_by :nick => current_user.name
+      puts params[:channel]
+      puts current_user.name
+      if @mods.channels.include? params[:channel] || params[:channel] == current_user.name
+        @logs = Logs.find_by :channel => params[:channel]
+      else
+        redirect_to '/'
+      end
     end
   end
 end
